015ca470bf7d251c2e757cddc7b414c19c5e309b
quynhbk <quynhle.lx@gmail.com>
2016-10-09 18:11:24 +0700

    add all

--- LearningCAdvance/LearningCAdvance/main.cpp
+++ LearningCAdvance/LearningCAdvance/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "Dongvat.h"
+#include <conio.h>
 
 int main() {
+	Dongvat *objDongVat;
+	objDongVat = new Dongvat();
 
+	objDongVat->Display();
+
+	objDongVat->setGioitinh(Dongvat::CON_CAI);
+	objDongVat->Display();
+
+	delete objDongVat;
+	
+	getch();
 	return 0;
 }
